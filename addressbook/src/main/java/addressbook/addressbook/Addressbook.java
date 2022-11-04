eea42c0d18d3dafe332451dfba9a5f580e93ca87
Momen-G-Ar-Official <cp.momen@gmail.com>
2022-11-04 13:14:58 +0200

    The start of the project

---
+++ addressbook/src/main/java/addressbook/addressbook/Addressbook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package addressbook.addressbook;
+
+/**
+ * @author Momin Arafa && Tamim Salhab
+ */
+public class Addressbook {
+
+    public static void main(String[] args) {
+        System.out.println("The start of the Project");
+    }
+}
